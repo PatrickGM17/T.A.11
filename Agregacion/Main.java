897244ca40f4e3b6e5a07975c1ddcd8d44aa8737
PatrickGM17 <103973835+PatrickGM17@users.noreply.github.com>
2022-06-13 18:04:17 -0500

    Add files via upload

---
+++ Agregacion/Main.java
@@ -0,0 +1,9 @@
+package TA11.Agregacion;
+
+public class Main {
+    public static void main(String[] args) {
+        Panal hexagonHonexIndustries;
+        hexagonHonexIndustries = new Panal();
+
+    }
+}
